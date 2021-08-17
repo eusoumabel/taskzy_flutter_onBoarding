a121299e3138bc8c89c3da20ad21a4fb62c3f331
estermabel <estermabel2@gmail.com>
2021-08-17 16:28:02 -0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Taskzy',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: kAccentColor,
